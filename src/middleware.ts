fccf295f54a705bea4720d2481d39f67cb643ea9
Shaukat Ali <Shaukatalibans@gmail.com>
2025-01-31 16:09:40 +0500

    Added next middleware for private and protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./lib/auth.config";
+
+export default NextAuth(authConfig).auth;
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         */
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    ],
+};
