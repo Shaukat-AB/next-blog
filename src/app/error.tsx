874d5dcae83a96f7539a39767398866c2e796dbb
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-05 12:15:03 +0500

    Added reset function to Error page and updated types.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,13 @@
 
 import Link from "next/link";
 
-const ErrorPage = ({ error, rest }) => {
+const ErrorPage = ({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) => {
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="py-8 lg:py-16">
@@ -10,11 +16,14 @@ const ErrorPage = ({ error, rest }) => {
                     <h1 className="main-title mb-4">Something went wrong!</h1>
                     <p className="text-2xl mb-4">{error.message}</p>
                     <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-                        Try Refreshing the page or go to the home page.
+                        <Link href="/" className="link">
+                            go to the home page.
+                        </Link>
                     </p>
-                    <Link href="/" className="btn-primary">
-                        Back to Homepage
-                    </Link>
+
+                    <button onClick={() => reset()} className="btn-primary">
+                        Try again
+                    </button>
                 </div>
             </div>
         </section>
