81b57d30febcc58fb26477e9cb8434ec856cefdb
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-05 11:50:40 +0500

    Defined type for Table props.

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,4 +1,12 @@
-const Table = ({ children, tableHead = [] }) => {
+import { ReactNode } from "react";
+
+const Table = ({
+    children,
+    tableHead = [],
+}: {
+    children: any;
+    tableHead: string[];
+}) => {
     return (
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
